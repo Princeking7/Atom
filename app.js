0e7f21bdae6b3854983d955caa7a54279bc8b7a6
Emmanuel Ebuka <eprinceembrus@gmail.com>
2020-07-17 21:14:09 +0100

    updated

--- app.js
+++ app.js
@@ -4,14 +4,14 @@
  var port = 3000;
 
 //static files
-app.use(express.static("public"))
-app.use("/css", express.static(__dirname + "public/css"))
-app.use("/js", express.static(__dirname + "public/js"))
-app.use("/img", express.static(__dirname + "public/img"))
+app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "public/css"));
+app.use("/js", express.static(__dirname + "public/js"));
+app.use("/img", express.static(__dirname + "public/img"));
 
 //set views
-app.set("views", "./views")
-app.set("view engine", "ejs")
+app.set("views", "./views");
+app.set("view engine", "ejs");
 
 app.get("", function(req,res){
 res.render("index", {text: "this is ejs"});}
